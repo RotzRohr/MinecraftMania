6c94ad0082524b0eb6ac2b9a037ff6227948f0cc
Linus <gsd@sa>
2022-12-24 16:08:07 +0100

    Updated so you can rejoin and be still in a team

---
+++ src/main/java/minecraftmania/minecraftmania/games/Parkour.java
@@ -0,0 +1,21 @@
+package minecraftmania.minecraftmania.games;
+
+import minecraftmania.minecraftmania.board.FastBoard;
+
+public class Parkour implements Game
+{
+    @Override
+    public void updateBoard(FastBoard board) {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
